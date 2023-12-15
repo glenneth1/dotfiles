73d9ace3fb5d9235b4d16fd9848c8672537d89b7
glenneth1 <glennt40@gmail.com>
2023-12-15 21:06:28 +0300

    Restored deleted init.el and early-init.el files (recovered)

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,1 +1,11 @@
-/home/glenn/.dotfiles/.emacs.d/early-init.el
+;;; early-init.el --- Emacs early initialization for Crafted Emacs (optional) -*- lexical-binding: t; -*-
+;;; Commentary:
+;;
+;;; Code:
+
+;; Configures `package.el'
+(load "~/.emacs.d/modules/crafted-early-init-config")
+
+(provide 'early-init)
+
+;;; early-init.el ends here
