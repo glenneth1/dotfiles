1e7912aa6d1d3c56f09df86c20b2c50432a5cdfd
glenneth1 <glennt40@gmail.com>
2024-01-06 13:47:43 +0300

    Added org-capture keybinding, not working rn

--- .config/nyxt/auto-config.4.lisp
+++ .config/nyxt/auto-config.4.lisp
@@ -21,3 +21,20 @@
   ((default-modes
     (remove-if (lambda (nyxt::m) (string= (symbol-name nyxt::m) "DARK-MODE"))
                %slot-value%))))
+
+(defvar *my-keymap* (make-keymap "my-map")
+  "Keymap for `my-mode'.")
+
+(define-command org-capture (&optional (buffer (current-buffer)))
+  "Org-capture current page."
+  (eval-in-emacs
+   `(org-link-set-parameters
+     "next"
+     :store (lambda ()
+              (org-store-link-props
+               :type "next"
+               :link ,(url buffer)
+               :description ,(title buffer))))
+   `(org-capture)))
+(define-key *my-keymap* "C-M-o" 'org-capture)
+
